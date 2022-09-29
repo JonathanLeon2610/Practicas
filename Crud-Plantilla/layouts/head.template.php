cd7055202b83d57b6a4eb7a1cfe1f8e03668d2f4
Jonathan Leon <jonathan2610leon@gmail.com>
2022-09-28 20:52:35 -0600

    Update head.template.php

--- Crud-Plantilla/layouts/head.template.php
+++ Crud-Plantilla/layouts/head.template.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+include "../app/ChargeProductController.php";
+$products= new ChargeProductController();
+$products=$products-> chargeProducts($_SESSION["token"]);
+?>
 <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
